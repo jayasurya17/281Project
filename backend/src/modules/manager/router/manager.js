f23941ee333a44c4e41055bfbf84dbafdb06e166
jayasurya17 <jayasurya1796@gmail.com>
2020-04-10 14:35:43 -0700

    Merging files after view runs

--- backend/src/modules/manager/router/manager.js
+++ backend/src/modules/manager/router/manager.js
@@ -3,12 +3,9 @@
 import express from 'express'
 let router = express.Router()
 import managerController from '../controller/manager'
-//import managerController from '../controller/manager2'
 import upload from '../../../utils/upload'
 
-//router.post('/addProject', createBucket, upload.single('file'), managerController.addProject)
 router.post('/addProject', upload.single('file'), managerController.addProject)
-//router.post('/addProject2',managerController.addProject2)
 router.post('/viewProject', managerController.viewProject)
 router.get('/allProjects/:managerId', managerController.allProjects)
 
