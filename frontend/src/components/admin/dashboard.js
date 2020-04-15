0552eb2e047cb0dbce2b1c23801cdc351673bce4
jayasurya17 <jayasurya1796@gmail.com>
2020-04-14 22:56:44 -0700

    Dashboard for project added

--- frontend/src/components/admin/dashboard.js
+++ frontend/src/components/admin/dashboard.js
@@ -21,37 +21,37 @@ class Home extends Component {
                         <DashboardCard heading="Number of testers" count="7" background="bg-warning" />
                     </div>
                     <div className="col-md-4">
-                        <DashboardCard heading="Number of projects" count="9" background="bg-success" />
+                        <DashboardCard heading="Number of projects" count="37" background="bg-success" />
                     </div>
                 </div>
                 
                 <div className="row mt-5">
                     <div className="col-md-3">
-                        <DashboardCard heading="Pre booked real devices" count="5" background="bg-primary" />
+                        <DashboardCard heading="Pre booked real devices" count="0" background="bg-primary" />
                     </div>
                     <div className="col-md-3">
-                        <DashboardCard heading="Devices being used on-demand" count="7" background="bg-secondary" />
+                        <DashboardCard heading="Devices being used on-demand" count="17" background="bg-secondary" />
                     </div>
                     <div className="col-md-3">
                         <DashboardCard heading="Active runs on real devices" count="9" background="bg-warning" />
                     </div>
                     <div className="col-md-3">
-                        <DashboardCard heading="Completed runs real devices" count="9" background="bg-success" />
+                        <DashboardCard heading="Completed runs real devices" count="59" background="bg-success" />
                     </div>
                 </div>
                 
                 <div className="row mt-5 mb-5">
                     <div className="col-md-3">
-                        <DashboardCard heading="Pre booked simulators by managers" count="5" background="bg-primary" />
+                        <DashboardCard heading="Pre booked simulators by managers" count="0" background="bg-primary" />
                     </div>
                     <div className="col-md-3">
-                        <DashboardCard heading="Simulators being used on-demand" count="7" background="bg-secondary" />
+                        <DashboardCard heading="Simulators being used on-demand" count="17" background="bg-secondary" />
                     </div>
                     <div className="col-md-3">
-                        <DashboardCard heading="Active runs on simulators" count="9" background="bg-warning" />
+                        <DashboardCard heading="Active runs on simulators" count="18" background="bg-warning" />
                     </div>
                     <div className="col-md-3">
-                        <DashboardCard heading="Completed runs simulators" count="9" background="bg-success" />
+                        <DashboardCard heading="Completed runs simulators" count="31" background="bg-success" />
                     </div>
                 </div>
 
