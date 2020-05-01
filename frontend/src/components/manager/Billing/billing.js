71dcfd1d44d0c0625296c74345764d1f7997c5c7
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-04-30 17:46:34 -0700

    added graphs and emulator run times

--- frontend/src/components/manager/Billing/billing.js
+++ frontend/src/components/manager/Billing/billing.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Header from '../../common/header';
 import Footer from '../../common/footer';
 import Navigation from '../../common/navigation';
@@ -18,75 +18,75 @@ import { Card, CardBody, CardHeader, CardText, CardTitle, Container, CardImg } f
 
 class Landing extends Component {
 
-    render(){
+    render() {
 
-        return(
+        return (
             <div>
-    
+
                 {/* <!-- Card with information --> */}
                 <div class="bg-white pl-5 pr-5 pb-5">
                     <Header />
                     <Navigation />
                     <div class="card-arrange">
-							<Container>
+                        <Container>
                             <Card className="card">
-                            <CardImg top width="100%" src={logo1} alt="Card image cap" />
+                                <CardImg top width="100%" src={logo1} alt="Card image cap" />
 
-								<CardHeader>Computation Pricing</CardHeader>
+                                <CardHeader>Computation Pricing</CardHeader>
 
-								<CardBody>
-									<CardText><b>Base Price:</b> 3 Dollars </CardText>
-									<CardText><b>Compute Power</b> 1 GB Ram</CardText>
-                                    </CardBody>
-							</Card>
+                                <CardBody>
+                                    <CardText><b>Base Price:</b> 3 Dollars </CardText>
+                                    <CardText><b>Compute Power</b> 1 GB Ram</CardText>
+                                </CardBody>
+                            </Card>
                             <Card className="card">
-                            <CardImg top width="100%" src={logo2} alt="Card image cap" />
+                                <CardImg top width="100%" src={logo2} alt="Card image cap" />
 
-								<CardHeader>Storage Cost</CardHeader>
-								<CardBody>
-									<CardText><b>Base Price:</b> 50 cents </CardText>
-									<CardText><b>Price per File</b> 15 cents</CardText>
-                                    </CardBody>
-							</Card>
+                                <CardHeader>Storage Cost</CardHeader>
+                                <CardBody>
+                                    <CardText><b>Base Price:</b> 50 cents </CardText>
+                                    <CardText><b>Price per File</b> 15 cents</CardText>
+                                </CardBody>
+                            </Card>
                             <Card className="card">
-                            <CardImg top width="100%" src={logo3} alt="Card image cap" />
+                                <CardImg top width="100%" src={logo3} alt="Card image cap" />
 
-								<CardHeader>Emulator Pricing</CardHeader>
-								<CardBody>
-									<CardText><b>Cost for each emulator run</b> 50 Cents</CardText>
-                                    </CardBody>
-							</Card>
+                                <CardHeader>Emulator Pricing</CardHeader>
+                                <CardBody>
+                                    <CardText><b>Cost for each emulator run</b> 50 Cents</CardText>
+                                </CardBody>
+                            </Card>
                             <Card className="card">
-                            <CardImg top width="100%" src={logo4} alt="Card image cap" />
+                                <CardImg top width="100%" src={logo4} alt="Card image cap" />
 
-								<CardHeader>Device Farm Pricing</CardHeader>
-								<CardBody>
-									<CardText><b>Base cost for each run:</b> 60 Cents </CardText>
-									<CardText><b>Price per minute</b> 10 Cents</CardText>
+                                <CardHeader>Device Farm Pricing</CardHeader>
+                                <CardBody>
+                                    <CardText><b>Base cost for each run:</b> 60 Cents </CardText>
+                                    <CardText><b>Price per minute</b> 10 Cents</CardText>
                                     <CardText><b>Price per device</b> 15 Cents</CardText>
-                                    </CardBody>
-							</Card>
+                                </CardBody>
+                            </Card>
                             <Card className="card">
-                            <CardImg top width="100%" src={logo5} alt="Card image cap" />
+                                <CardImg top width="100%" src={logo5} alt="Card image cap" />
 
-								<CardHeader>Mobile Taas Service Price</CardHeader>
-								<CardBody>
-									<CardTitle><b>Base Price:</b> 5 Dollars </CardTitle>
-									
-                                    </CardBody>
-							</Card>
+                                <CardHeader>Mobile Taas Service Price</CardHeader>
+                                <CardBody>
+                                    <CardTitle><b>Base Price:</b> 5 Dollars </CardTitle>
+
+                                </CardBody>
+                            </Card>
                             <Card className="card">
-                            <CardImg top width="100%" src={logo6} alt="Card image cap" />
+                                <CardImg top width="100%" src={logo6} alt="Card image cap" />
 
-								<CardHeader>Network Prices and Tax</CardHeader>
-								<CardBody>
-									<CardText><b>Network and Infrastructure Price:</b> 2 Dollars </CardText>
-									<CardText><b>Tax Pricing</b>:  1.3 Dollars</CardText>
-                                    </CardBody>
-							</Card>
-                            </Container>
-						</div>
-       
+                                <CardHeader>Network Prices and Tax</CardHeader>
+                                <CardBody>
+                                    <CardText><b>Network and Infrastructure Price:</b> 2 Dollars </CardText>
+                                    <CardText><b>Tax Pricing</b>:  1.3 Dollars</CardText>
+                                </CardBody>
+                            </Card>
+                        </Container>
+                    </div>
+
                     <div className="row p-5">
                         <div className="col-md-3 offset-md-8">
                             <a href="/manager/view/bill" target="_blank">
@@ -96,7 +96,7 @@ class Landing extends Component {
                     </div>
                     {/* https://reactjs.org/docs/dom-elements.html#style */}
                     <ViewBillTable />
-                    
+
                     <Footer />
                 </div>
             </div>
