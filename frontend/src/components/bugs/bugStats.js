0586454d1f3b021bf7b6777308dc0c28eeb8b992
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-05-02 18:32:35 -0700

    added bugs graphs

--- frontend/src/components/bugs/bugStats.js
+++ frontend/src/components/bugs/bugStats.js
@@ -1,10 +1,11 @@
-import  React, {Component} from 'react';
-import {Card, CardTitle} from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardTitle } from 'reactstrap';
 import Header from '../common/header';
 import Footer from '../common/footer';
 import Navigation from '../common/navigation';
 import axios from 'axios';
 import Constants from '../../utils/constants';
+import { VictoryPie, VictoryChart, VictoryBar, VictoryTheme } from 'victory'
 
 
 class BugStats extends Component {
@@ -12,34 +13,34 @@ class BugStats extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            projectId : "5e8fabd02d11df46cc4c69a5",
-            bugStats : {
+            projectId: "5e8fabd02d11df46cc4c69a5",
+            bugStats: {
 
             }
         };
-      }
+    }
 
     componentDidMount() {
         axios.get(`${Constants.BACKEND_SERVER.URL}/bugs/getBugStatsByProject/${this.state.projectId}`)
-        .then(response =>{
-            this.setState({bugStats:response.data});
-            console.log(this.state.bugStats);
-            //should print 
-            // {
-            //     "total": 6,
-            //     "open": 5,
-            //     "closed": 1,
-            //     "severity": {
-            //         "high": 2,
-            //         "medium": 3,
-            //         "low": 1
-            //     }
-            // }
-            // HANDLE response here
-        })
-        .catch(err => {
-            console.log(err);
-            let data =  {
+            .then(response => {
+                this.setState({ bugStats: response.data });
+                console.log(this.state.bugStats);
+                //should print 
+                // {
+                //     "total": 6,
+                //     "open": 5,
+                //     "closed": 1,
+                //     "severity": {
+                //         "high": 2,
+                //         "medium": 3,
+                //         "low": 1
+                //     }
+                // }
+                // HANDLE response here
+            })
+            .catch(err => {
+                console.log(err);
+                let data = {
                     "total": 0,
                     "open": 0,
                     "closed": 0,
@@ -49,27 +50,75 @@ class BugStats extends Component {
                         "low": 0
                     }
                 }
-            this.setState({bugStats:data});
-        })
+                this.setState({ bugStats: data });
+            })
     }
 
-    render(){
+    render() {
+        let bugsCount, bugsAnalysis
+        if (this.state.bugStats !== null && this.state.bugStats.open >= 0 && this.state.bugStats.closed >= 0) {
+            bugsCount =
+                <div>
+                    <p className='display-4'>Bugs count</p>
+                    <svg width={450} height={450}>
+                        <VictoryPie
+                            standalone={false}
+                            animate={{
+                                duration: 2000,
+                                onLoad: { duration: 1000 }
+                            }}
+                            width={400} height={400}
+                            innerRadius={100}
+                            colorScale={["#e42024", "#a7d930"]}
+                            padAngle={({ datum }) => datum.y}
+                            data={[
+                                { x: 1, y: this.state.bugStats.open, label: `Open:${this.state.bugStats.open}` },
+                                { x: 2, y: this.state.bugStats.closed, label: `Closed:${this.state.bugStats.closed}` },
+                            ]}
+                        />
+                    </svg>
 
-        return(
+
+                </div>
+        }
+
+        if (this.state.bugStats.severity && this.state.bugStats.severity.high >= 0 && this.state.bugStats.severity.medium >= 0 && this.state.bugStats.severity.low >= 0) {
+            bugsAnalysis =
+                <div>
+                    <p className='display-4'>Bugs Analysis</p>
+                    <svg width={450} height={450}>
+                        <VictoryPie
+                            standalone={false}
+                            animate={{
+                                duration: 2000,
+                                onLoad: { duration: 1000 }
+                            }}
+                            width={400} height={400}
+                            innerRadius={100}
+                            colorScale={["#e42024", "#ef545f", "#e9b1b1"]}
+                            padAngle={({ datum }) => datum.y}
+                            data={[
+                                { x: 1, y: this.state.bugStats.severity.high, label: `High:${this.state.bugStats.severity.high}` },
+                                { x: 2, y: this.state.bugStats.severity.medium, label: `Medium:${this.state.bugStats.severity.medium}` },
+                                { x: 3, y: this.state.bugStats.severity.low, label: `Low:${this.state.bugStats.severity.low}` },
+
+
+
+                            ]}
+                        />
+                    </svg>
+
+
+                </div>
+        }
+
+        return (
             <div>
                 <div class="bg-white pl-5 pr-5 pb-5">
-                    <Header />
-                    <Navigation />
-                    <div style={{display : "flex",flexDirection :"column",paddingTop:"50px"}}>
-                        <Card>
-                            <CardTitle>Total : {this.state.bugStats.total}</CardTitle>
-                            <CardTitle>Open : {this.state.bugStats.open}</CardTitle>
-                            <CardTitle>Closed : {this.state.bugStats.closed}</CardTitle>
-                            <CardTitle>High : {this.state.bugStats.severity && this.state.bugStats.severity.high}</CardTitle>
-                            <CardTitle>Medium : {this.state.bugStats.severity && this.state.bugStats.severity.medium}</CardTitle>
-                            <CardTitle>Low : {this.state.bugStats.severity && this.state.bugStats.severity.low}</CardTitle>
-                        </Card>
-                        <Footer />
+
+                    <div style={{ display: "flex", paddingTop: "50px" }}>
+                        {bugsCount}
+                        {bugsAnalysis}
                     </div>
                 </div>
             </div>
