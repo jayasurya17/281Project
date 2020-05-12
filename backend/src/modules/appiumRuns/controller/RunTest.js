e3c0e5b3ea1e22ad5e660fa5f7a3833aad8c990f
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-05-12 14:29:06 -0700

    emulator fixes

--- backend/src/modules/appiumRuns/controller/RunTest.js
+++ backend/src/modules/appiumRuns/controller/RunTest.js
@@ -4,6 +4,7 @@ const wdio = require('webdriverio');
 const assert = require('assert');
 const fs = require('fs')
 
+const child_process = require('child_process');
 
 // const opts = {
 //   port: 4723,
@@ -20,40 +21,74 @@ const fs = require('fs')
 // };
 
 exports.runAppium = async (req) => {
-	req = req.capabilities;
-	const opts = {
-		port: 4723,
-		path: '/wd/hub/',
-		capabilities: {
-			platformName: req.platformName,
-			platformVersion: req.platformVersion,
-			deviceName: req.deviceName,
-			app: `./${req.app}`,
-			appPackage: req.appPackage,
-			appActivity: req.appActivity,
-			automationName: req.automationName,
-		},
-	};
-	console.log("current path " + process.cwd())
-	const client = await wdio.remote(opts);
+	return new Promise(async (resolve) => {
 
-	const field = await client.$('android.widget.EditText');
-	await field.setValue('Hello World!');
-	const value = await field.getText();
+		req = req.capabilities;
+		const ops = {
+			port: 4723,
+			path: '/wd/hub/',
+			capabilities: {
+				platformName: req.platformName,
+				platformVersion: req.platformVersion,
+				deviceName: req.deviceName,
+				app: `./${req.app}`,
+				appPackage: req.appPackage,
+				appActivity: req.appActivity,
+				automationName: req.automationName,
+			},
+		};
 
-	// let screenshot = client.takeScreenshot();
+		let cmd = 'node'
+		// let cmddata;
+		console.log("Running TestScript");
+		// console.log(req)
+		console.log(`./src/apkStore/${req.testScriptName}`)
+		let cp = await child_process.spawnSync(cmd, [`src/apkStore/${req.testScriptName}`,
+			"--platformName", `${req.platformName}`,
+			"--platformVersion", `${req.platformVersion}`,
+			"--deviceName", `${req.deviceName}`,
+			"--app", `./${req.app}`,
+			"--appPackage", `${req.appPackage}`,
+			"--appActivity", `${req.appActivity}`,
+			"--automationName", `${req.automationName}`,
+			"--port", "4723",
+			"--path", '/wd/hub/',
+		])
 
-	assert.equal(value, 'Hello World!');
-	let bugreport = await client.getLogs('bugreport');
-	let logs = await client.getLogs('logcat');
-	const date = new Date();
-	const fileName = req.runId + "_" + (date.getMonth() + 1) + "" + date.getDate() + "" + date.getFullYear() + "" + date.getHours() + "" + date.getMinutes() + "_Logs.txt";
-	let path = `./src/apkStore/${fileName}`
-	//JSON.stringify(logs.map(entry => entry.message).join('\n'))
-	try {
-		fs.writeFileSync(path, JSON.stringify(logs.map(entry => entry.message).join('\n')));
-	} catch (err) {
-		console.error(err)
-	}
-	await client.deleteSession();
+		console.log(JSON.stringify(cp.output))
+
+
+		resolve(cp)
+	});
+
+	// cp.stdout.on('data', (data) => {
+	// 	console.log(data.toString());
+
+	// })
+	// cp.stderr.on('data', (data) => {
+	// 	console.log(data.toString());
+	// })
+
+	// console.log("current path " + process.cwd())
+	// const client = await wdio.remote(opts);
+
+	// const field = await client.$('android.widget.EditText');
+	// await field.setValue('Hello World!');
+	// const value = await field.getText();
+
+	// // let screenshot = client.takeScreenshot();
+
+	// assert.equal(value, 'Hello World!');
+	// let bugreport = await client.getLogs('bugreport');
+	// let logs = await client.getLogs('logcat');
+	// const date = new Date();
+	// const fileName = req.runId + "_" + (date.getMonth() + 1) + "" + date.getDate() + "" + date.getFullYear() + "" + date.getHours() + "" + date.getMinutes() + "_Logs.txt";
+	// let path = `./src/apkStore/${fileName}`
+	// //JSON.stringify(logs.map(entry => entry.message).join('\n'))
+	// try {
+	// 	fs.writeFileSync(path, JSON.stringify(logs.map(entry => entry.message).join('\n')));
+	// } catch (err) {
+	// 	console.error(err)
+	// }
+	// await client.deleteSession();
 };
