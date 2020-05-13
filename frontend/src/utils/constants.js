68475e1a9999f0de3ba3f2973a2481c90391be79
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-05-12 19:16:59 -0700

    fixes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,7 +1,7 @@
 module.exports = {
     BACKEND_SERVER: {
-        URL: 'http://localhost:9000', // Should have http://
-        //URL: 'http://3.92.45.135:9000', // Should have http://
+        // URL: 'http://localhost:9000', // Should have http://
+        URL: 'http://281BCLB-1773563148.us-east-1.elb.amazonaws.com', // Should have http://
     },
     USER_INFORMATION: {
         USER_ID: localStorage.getItem('226UserId'),
